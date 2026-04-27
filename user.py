ad8635ae8b20085b4164476f1d1eb0edf1188e95
BuchtaTomas <tovele.test@gmail.com>
2026-04-27 15:42:13 +0200

    adding commas into user.py

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
 def get_username(user_id):
     users = {
-        1: "tomas"
-        2: "lenka"
+        1: "tomas",
+        2: "lenka",
         3: "vendulka"
     }
     return users.get(user_id, None)
